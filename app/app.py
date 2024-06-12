e5b3ce9f2d4954ad2b473dfd0a703c3761ce5cf8
iio1989 <iio-t@sic-system.com>
2024-06-13 01:26:26 +0900

    fixed history date & docker port.

--- app/app.py
+++ app/app.py
@@ -66,4 +66,4 @@ def history():
 
 if __name__ == '__main__':
     ptvsd.enable_attach(address=('0.0.0.0', 5679))
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=8000)
